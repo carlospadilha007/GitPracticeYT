7af0c5c374c339872805ff037c665a3e8ab3f9c3
Carlos A.  S. Padilha <cpadilha38@outlook.com.br>
2023-05-19 17:47:38 -0300

    Revert "Este é o nosso quarto commit"
    This reverts commit fcb600b8276b81e71c0a92fa8448c3b37b498eab.

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Este é o conteudo do meu segundo commit")
-
-        asjhakkjakakjlhjadjjakjkdajk
+        hoje é um bom dia
     }
 }
