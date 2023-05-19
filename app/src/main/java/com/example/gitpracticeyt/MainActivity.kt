fcb600b8276b81e71c0a92fa8448c3b37b498eab
Carlos A.  S. Padilha <cpadilha38@outlook.com.br>
2023-05-19 17:46:46 -0300

    Este é o nosso quarto commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Este é o conteudo do meu segundo commit")
-        hoje é um bom dia
+
+        asjhakkjakakjlhjadjjakjkdajk
     }
 }
