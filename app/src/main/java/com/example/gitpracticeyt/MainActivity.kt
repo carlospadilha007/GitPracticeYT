d91ad80610feece39ddffd4d936a6b81c16a194c
Carlos A.  S. Padilha <cpadilha38@outlook.com.br>
2023-05-20 14:33:01 -0300

    Alteração 2 commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Este é o conteudo do meu segundo commit")
-        println("Experimental")
+        println("Alteração 2")
     }
 }
