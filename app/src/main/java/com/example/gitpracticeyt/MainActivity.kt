01e035854205b9fc7e5784b35719d1b11c4422fe
Carlos A.  S. Padilha <cpadilha38@outlook.com.br>
2023-05-20 14:16:31 -0300

    Experimental commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Este é o conteudo do meu segundo commit")
-        hoje é um bom dia
+        println("Experimental")
     }
 }
